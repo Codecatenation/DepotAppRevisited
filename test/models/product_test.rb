faf69c3c40c5cd8ad49f4708cf65ad8c427b2a30
Codecatenation, LLC <codecatenation@gmail.com>
2015-08-15 19:19:06 -0400

    Validation!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes must not be empty" do
+  	product = Product.new
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:description].any?
+  	assert product.errors[:price].any?
+  	assert product.errors[:image_url].any?
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    asser product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], 
+    produc.terrors[:title]
+  end
 end
